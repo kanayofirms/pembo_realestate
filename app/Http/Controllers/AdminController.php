ce421b749045222af0d8572411f7bfac0129b19d
kanayofirms <kanyeze@gmail.com>
2024-10-08 17:13:42 +0100

    Refactor code for scalability

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function AdminDashboard(){
-        return view('admin.admin_dashboard');
+    public function AdminDashboard()
+    {
+        return view('admin.index');
     }
 }
