d339137b0c5c1afea5de066f780616fdef32ca31
kanayofirms <kanyeze@gmail.com>
2024-10-08 17:44:31 +0100

    fix(admin/controller): update AdminLogout function for secure session handling

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
         return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
