2efd887a265c7cd85867d3b2364e5ed8f0c203bc
kanayofirms <kanyeze@gmail.com>
2024-10-10 07:56:41 +0100

    feat(routes): add admin profile route

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ require __DIR__ . '/auth.php';
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('admin/profile', [AdminController::class, 'admin_profile']);
 
 });
 
