1798f18cf0077cd8ed3119a7669d63f46b03ca34
kanayofirms <kanyeze@gmail.com>
2024-10-14 13:03:25 +0100

    feat(routes): add route for admin users list

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
     Route::get('admin/profile', [AdminController::class, 'admin_profile']);
     Route::post('admin_profile/update', [AdminController::class, 'update']);
+    Route::get('admin/users', [AdminController::class, 'users']);
 
 });
 
