414b380a565192f24c7c5df6097687f5f6c9fdd0
kanayofirms <kanyeze@gmail.com>
2024-10-17 00:57:19 +0100

    feat(controllers): Implemented email composition saving functionality in EmailController

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\ComposeEmailModel;
 
 class EmailController extends Controller
 {
     public function compose()
     {
-        return view('admin.email.compose');
+        // $data['getEmail'] = User::where('role', '=', array(['agent', 'user']))->get();
+        $data['getEmail'] = User::whereIn('role', ['agent', 'user'])->get();
+        return view('admin.email.compose', $data);
+    }
+
+    public function post(Request $request)
+    {
+        // dd($request->all());
+        $save = new ComposeEmailModel;
+        $save->user_id = $request->user_id;
+        $save->cc_email = trim($request->cc_email);
+        $save->subject = trim($request->subject);
+        $save->descriptions = trim($request->descriptions);
+        $save->save();
+
+        return redirect('admin/email/compose')->with('success', "Email Successfully Sent!");
+
+
     }
 }
+
