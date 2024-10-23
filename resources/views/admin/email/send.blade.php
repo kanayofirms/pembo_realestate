6a46f2f4e81273e2dcaeb59d11049dcb0009bb43
kanayofirms <kanyeze@gmail.com>
2024-10-23 06:53:51 +0100

    feat(view/send): add session message display

--- resources/views/admin/email/send.blade.php
+++ resources/views/admin/email/send.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.admin_dashboard')
 @section('admin')
     <div class="page-content">
-
+        @include('_message')
         <div class="row inbox-wrapper">
             <div class="col-lg-12">
                 <div class="card">
@@ -124,65 +124,86 @@
                                         <div class="btn-group me-2">
                                             <button class="btn btn-outline-primary" type="button">Archive</button>
                                             <button class="btn btn-outline-primary" type="button">Span</button>
-                                            <button class="btn btn-outline-primary" type="button">Delete</button>
-                                        </div>
-                                        <div class="btn-group me-2 d-none d-xl-block">
-                                            <button class="btn btn-outline-primary dropdown-toggle"
-                                                data-bs-toggle="dropdown" type="button">Order by <span
-                                                    class="caret"></span></button>
-                                            <div class="dropdown-menu" role="menu">
-                                                <a class="dropdown-item" href="#">Date</a>
-                                                <a class="dropdown-item" href="#">From</a>
-                                                <a class="dropdown-item" href="#">Subject</a>
-                                                <div class="dropdown-divider"></div>
-                                                <a class="dropdown-item" href="#">Size</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex align-items-center justify-content-end flex-grow-1">
-                                        <span class="me-2">1-10 of 253</span>
-                                        <div class="btn-group">
-                                            <button class="btn btn-outline-secondary btn-icon" type="button"><i
-                                                    data-feather="chevron-left"></i></button>
-                                            <button class="btn btn-outline-secondary btn-icon" type="button"><i
-                                                    data-feather="chevron-right"></i></button>
+                                            <a href=""
+                                                onclick="return confirm('Are you sure you want to delete?');"
+                                                class="btn btn-outline-primary" id="getDeleteUrl" ">Delete</a>
+                                                        </div>
+                                                        <div class="btn-group me-2 d-none d-xl-block">
+                                                            <button class="btn btn-outline-primary dropdown-toggle"
+                                                                data-bs-toggle="dropdown" type="button">Order by <span
+                                                                    class="caret"></span></button>
+                                                            <div class="dropdown-menu" role="menu">
+                                                                <a class="dropdown-item" href="#">Date</a>
+                                                                <a class="dropdown-item" href="#">From</a>
+                                                                <a class="dropdown-item" href="#">Subject</a>
+                                                                <div class="dropdown-divider"></div>
+                                                                <a class="dropdown-item" href="#">Size</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="d-flex align-items-center justify-content-end flex-grow-1">
+                                                        <span class="me-2">1-10 of 253</span>
+                                                        <div class="btn-group">
+                                                            <button class="btn btn-outline-secondary btn-icon" type="button"><i
+                                                                    data-feather="chevron-left"></i></button>
+                                                            <button class="btn btn-outline-secondary btn-icon" type="button"><i
+                                                                    data-feather="chevron-right"></i></button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="email-list">
+
+                                                    <!-- email list item -->
+                                                        @foreach ($getRecord as $value)
+                                                <div class="email-list-item email-list-item--unread">
+                                                    <div class="email-list-actions">
+                                                        <div class="form-check">
+                                                            <input type="checkbox"
+                                                                class="form-check-input delete-all-option"
+                                                                value="{{ $value->id }}">
+                                                        </div>
+                                                        <a class="favorite" href="javascript:;"><span><i
+                                                                    data-feather="star"></i></span></a>
+                                                    </div>
+                                                    <a href="" class="email-list-detail">
+                                                        <div class="content">
+                                                            <span class="from">{{ $value->subject }}</span>
+                                                            <p class="msg">{{ $value->descriptions }}</p>
+                                                        </div>
+                                                        <span class="date">
+
+                                                            {{ date('d M', strtotime($value->created_at)) }}
+                                                        </span>
+                                                    </a>
+                                                </div>
+                                                @endforeach
+
+                                                {{-- end --}}
                                         </div>
                                     </div>
                                 </div>
-                                <div class="email-list">
 
-                                    <!-- email list item -->
-                                    @foreach ($getRecord as $value)
-                                        <div class="email-list-item email-list-item--unread">
-                                            <div class="email-list-actions">
-                                                <div class="form-check">
-                                                    <input type="checkbox" class="form-check-input">
-                                                </div>
-                                                <a class="favorite" href="javascript:;"><span><i
-                                                            data-feather="star"></i></span></a>
-                                            </div>
-                                            <a href="" class="email-list-detail">
-                                                <div class="content">
-                                                    <span class="from">{{ $value->subject }}</span>
-                                                    <p class="msg">{{ $value->descriptions }}</p>
-                                                </div>
-                                                <span class="date">
-
-                                                    {{ date('d M', strtotime($value->created_at)) }}
-                                                </span>
-                                            </a>
-                                        </div>
-                                    @endforeach
-
-                                    {{-- end --}}
-                                </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
-            </div>
-        </div>
 
-    </div>
-@endsection
+            </div>
+        @endsection
+
+        @section('script')
+            <script type="text/javascript">
+                $('.delete-all-option').change(function() {
+                    var total = '';
+                    $('.delete-all-option').each(function() {
+                        if (this.checked) {
+                            var id = $(this).val();
+                            total += id + ',';
+                        }
+                    });
+
+                    var url = '{{ url('admin/email_sent?id=') }}' + total;
+                    $('#getDeleteUrl').attr('href', url);
+                });
+            </script>
+        @endsection
