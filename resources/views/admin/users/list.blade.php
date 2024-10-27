45f2d92caa6176616d864ee2026dced33b1fa04d
kanayofirms <kanyeze@gmail.com>
2024-10-27 02:49:25 +0100

    feat(admin.users.list): Add search filter form

--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -7,6 +7,88 @@
                 <li class="breadcrumb-item active" aria-current="page">Users List</li>
             </ol>
         </nav>
+
+        {{-- Search Box start --}}
+        <div class="row">
+            <div class="col-lg-12 stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="card-title">Search Users</h6>
+                        <form action="">
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Id</label>
+                                        <input type="text" name="" class="form-control" placeholder="Enter id">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Name</label>
+                                        <input type="text" name="name" class="form-control" placeholder="Enter name">
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Username</label>
+                                        <input type="text" name="username" class="form-control"
+                                            placeholder="Enter username">
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Email Id</label>
+                                        <input type="text" name="email" class="form-control"
+                                            placeholder="Enter email id">
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Phone Number</label>
+                                        <input type="text" name="phone" class="form-control"
+                                            placeholder="Enter phone number">
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Address</label>
+                                        <input type="text" name="address" class="form-control"
+                                            placeholder="Enter address">
+                                    </div>
+                                </div>
+                                <div class="col-sm-2">
+                                    <div class="mb-3">
+                                        <label class="form-label">Role</label>
+                                        <select class="form-control" name="role">
+                                            <option value="">Select role</option>
+                                            <option value="admin">Admin</option>
+                                            <option value="agent">Agent</option>
+                                            <option value="user">User</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-sm-2">
+                                    <div class="mb-3">
+                                        <label class="form-label">Status</label>
+                                        <select class="form-control" name="status">
+                                            <option value="">Select status</option>
+                                            <option value="active">Active</option>
+                                            <option value="inactive">Inactive</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Search</button>
+                            <a href="{{ url('admin/users') }}" class="btn btn-danger">Reset</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+
+        {{-- Search Box end --}}
+
         <div class="row">
             <div class="col-lg-12 stretch-card">
                 <div class="card">
