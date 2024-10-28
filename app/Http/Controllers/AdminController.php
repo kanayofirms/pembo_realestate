bc909dbeba5f75643e9efc589745daf63050ae15
kanayofirms <kanyeze@gmail.com>
2024-10-28 14:19:13 +0100

    feat(AdminController): Implement user creation logic

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -87,4 +87,28 @@ class AdminController extends Controller
     {
         return view('admin.users.add');
     }
+
+    public function admin_add_users_store(Request $request)
+    {
+        // dd($request->all());
+        $user = request()->validate([
+            'name' => 'required',
+            'username' => 'required',
+            'email' => 'required|unique:users',
+            'role' => 'required',
+            'status' => 'required'
+        ]);
+
+        $user = new User;
+        $user->name = trim($request->name);
+        $user->username = trim($request->username);
+        $user->email = trim($request->email);
+        $user->phone = trim($request->phone);
+        $user->role = trim($request->role);
+        $user->status = trim($request->status);
+
+        $user->save();
+
+        return redirect('admin/users')->with('success', "Record successfully added.");
+    }
 }
