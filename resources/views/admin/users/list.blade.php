4eb17c90ff549de31c8140e4e95f21038d52b405
kanayofirms <kanyeze@gmail.com>
2024-10-30 09:55:33 +0100

    feat(admin.users.list): Added delete button with SVG icon for user management

--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -187,7 +187,7 @@
                                                         </path>
                                                     </svg> <span class="">Edit</span></a>
 
-                                                {{-- <a class="dropdown-item d-flex align-items-center"
+                                                <a class="dropdown-item d-flex align-items-center"
                                                     href="{{ url('admin/users/delete/' . $value->id) }}"
                                                     onclick="return confirm('Are you sure you want to delete?')"><svg
                                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -198,7 +198,7 @@
                                                         <path
                                                             d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
                                                         </path>
-                                                    </svg> <span class="">Delete</span></a> --}}
+                                                    </svg> <span class="">Delete</span></a>
                                             </td>
 
                                         </tr>
