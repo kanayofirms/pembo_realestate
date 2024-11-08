f91bc2e29f7dcf7edf154ede78a6430b515e4507
kanayofirms <kanyeze@gmail.com>
2024-11-08 02:19:28 +0100

    feat(controller): Add week store method

--- app/Http/Controllers/UserTimeController.php
+++ app/Http/Controllers/UserTimeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\WeekModel;
 
 class UserTimeController extends Controller
 {
@@ -14,5 +15,15 @@ class UserTimeController extends Controller
     public function week_add(Request $request)
     {
         return view('admin.week.add');
+    }
+
+    public function week_store(Request $request)
+    {
+        // dd($request->all());
+        $save = new WeekModel;
+        $save->name = trim($request->name);
+        $save->save();
+
+        return redirect('admin/week')->with('success', "Week Added Successfully.");
     }
 }
