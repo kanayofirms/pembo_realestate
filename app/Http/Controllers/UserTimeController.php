a9a4de1391764206e0034a25045a22953e71fc49
kanayofirms <kanyeze@gmail.com>
2024-11-08 02:54:37 +0100

    feat(controller): Update week list method to retrieve all weeks

--- app/Http/Controllers/UserTimeController.php
+++ app/Http/Controllers/UserTimeController.php
@@ -9,7 +9,8 @@ class UserTimeController extends Controller
 {
     public function week_list(Request $request)
     {
-        return view('admin.week.list');
+        $data['getRecord'] = WeekModel::get();
+        return view('admin.week.list', $data);
     }
 
     public function week_add(Request $request)
