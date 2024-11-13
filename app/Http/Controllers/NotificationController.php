c1b4885b743dc2f9cbb842ede9097303215e5a05
kanayofirms <kanyeze@gmail.com>
2024-11-13 09:52:49 +0100

    enhance(controller): Refactor notification send method for better performance and readability"

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Exception;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\NotificationModel;
 
 class NotificationController extends Controller
 {
@@ -16,27 +17,37 @@ class NotificationController extends Controller
 
     public function notification_send(Request $request)
     {
+        // dd($request->all());
+
+        $saveDb = new NotificationModel;
+        $saveDb->user_id = trim($request->user_id);
+        $saveDb->title = trim($request->title);
+        $saveDb->message = trim($request->message);
+        $saveDb->save();
+
         $user = User::where('id', '=', $request->user_id)->first();
         if (!empty($user->token)) {
 
         }
         try {
-            $serverKey = 'Please set your Firebase server key';
+            $accessToken = getenv('GOOGLE_APPLICATION_CREDENTIALS');
 
             $token = $user->token;
             $body['title'] = $request->title;
             $body['message'] = $request->message;
             $body['body'] = $request->message;
 
-            $url = "https://fcm.googleapis.com/fcm/send";
+            $url = "POST https://fcm.googleapis.com/v1/projects/pembo-realestate/messages:send";
 
             $notification = array('title' => $request->title, 'body' => $request->message);
 
             $arrayToSend = array('to' => $token, 'data' => $body, 'priority' => "high");
+
             $json1 = json_encode($arrayToSend);
             $headers = array();
             $headers[] = 'Content-Type: application/json';
-            $headers[] = 'Authorization: key=' . $serverKey;
+            $headers[] = 'Authorization: Bearer=' . $accessToken;
+
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
