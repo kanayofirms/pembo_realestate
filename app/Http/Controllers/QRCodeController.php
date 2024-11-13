d2690997dc13121f228281a9613e975e290caf29
kanayofirms <kanyeze@gmail.com>
2024-11-13 19:34:48 +0100

    feat(controller): Implement store qrcode method for product storage

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductModel;
 use Illuminate\Http\Request;
 
 class QRCodeController extends Controller
@@ -14,5 +15,19 @@ class QRCodeController extends Controller
     public function add_qrcode(Request $request)
     {
         return view('admin.qrcode.add');
+    }
+
+    public function store_qrcode(Request $request)
+    {
+        // dd($request->all());
+        $randNum = mt_rand(999999999, 11111111111);
+        $save = new ProductModel;
+        $save->title = trim($request->title);
+        $save->price = trim($request->price);
+        $save->product_code = $randNum;
+        $save->description = trim($request->description);
+        $save->save();
+
+        return redirect('admin/qrcode')->with('success', "QRCode Successfully Added");
     }
 }
