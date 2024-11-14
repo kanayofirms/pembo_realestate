982d52b422b4d965b0fd2aae9b28e482d4b246d4
kanayofirms <kanyeze@gmail.com>
2024-11-14 17:16:46 +0100

    feat(controller): Add SMTP settings update functionality

--- app/Http/Controllers/SMTPController.php
+++ app/Http/Controllers/SMTPController.php
@@ -15,6 +15,19 @@ class SMTPController extends Controller
 
     public function smtp_update(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
+        $save = SMTPModel::getSingleFirst();
+        $save->app_name = trim($request->app_name);
+        $save->mail_mailer = trim($request->mail_mailer);
+        $save->mail_host = trim($request->mail_host);
+        $save->mail_port = trim($request->mail_port);
+        $save->mail_username = trim($request->mail_username);
+        $save->mail_password = trim($request->mail_password);
+        $save->mail_encryption = trim($request->mail_encryption);
+        $save->mail_from_address = trim($request->mail_from_address);
+
+        $save->save();
+
+        return redirect('admin/smtp')->with('success', "SMTP Successfully Updated!");
     }
 }
