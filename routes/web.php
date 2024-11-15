9fb3e04a27b7c61298fe89e0fdd36ece6cc27a9b
kanayofirms <kanyeze@gmail.com>
2024-11-15 02:28:56 +0100

    feat(admin-colour): Create route for updating colour by ID

--- routes/web.php
+++ routes/web.php
@@ -104,6 +104,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('admin/colour/add', [ColourController::class, 'add_colour']);
     Route::post('admin/colour/add', [ColourController::class, 'store_colour']);
     Route::get('admin/colour/edit/{id}', [ColourController::class, 'edit_colour']);
+    Route::post('admin/colour/edit/{id}', [ColourController::class, 'update_colour']);
     // Colour End
 });
 
