ad9b646ed17b3a43c75285563e84524cdc566bb8
kanayofirms <kanyeze@gmail.com>
2024-11-19 11:29:01 +0100

    feat(model): enhance getRecord method to support search functionality

--- app/Models/OrdersModel.php
+++ app/Models/OrdersModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class OrdersModel extends Model
 {
@@ -11,10 +12,28 @@ class OrdersModel extends Model
 
     protected $table = 'orders';
 
-    static public function getRecord()
+    static public function getRecord($request)//update function with $request param for search
     {
         $return = self::select('orders.*', 'product.title');
         $return = $return->join('product', 'product.id', '=', 'orders.product_id');
+
+        //Search start
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('orders.id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('title'))) {
+            $return = $return->where('product.title', 'like', '%' . Request::get('title') . '%');
+        }
+
+        if (!empty(Request::get('created_at'))) {
+            $return = $return->where('orders.created_at', 'like', '%' . Request::get('created_at') . '%');
+        }
+
+        if (!empty(Request::get('updated_at'))) {
+            $return = $return->where('orders.updated_at', 'like', '%' . Request::get('updated_at') . '%');
+        }
+        //Search End
 
         $return = $return->orderBy('orders.id', 'desc')
             ->get();
