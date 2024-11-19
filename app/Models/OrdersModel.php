eb3f1de8206c52e9128a9dbf46e238fa1530d0ca
kanayofirms <kanyeze@gmail.com>
2024-11-19 09:58:54 +0100

    feat(model): Add getColour relationship to retrieve order colours

--- app/Models/OrdersModel.php
+++ app/Models/OrdersModel.php
@@ -20,4 +20,11 @@ class OrdersModel extends Model
             ->get();
         return $return;
     }
+
+    public function getColour()
+    {
+        return $this->hasMany(OrdersDetailsModel::class, 'orders_id')
+            ->select('orders_details.*', 'colour.name')
+            ->join('colour', 'colour.id', '=', 'orders_details.colour_id');
+    }
 }
