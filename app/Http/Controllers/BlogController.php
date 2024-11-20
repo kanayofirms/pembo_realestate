026994fc38c1db3933f9edf27109c29db447ea31
kanayofirms <kanyeze@gmail.com>
2024-11-20 09:53:55 +0100

    feat(controller): add method to store new blog entry in database

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BlogModel;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -14,5 +15,23 @@ class BlogController extends Controller
     public function add_blog(Request $request)
     {
         return view('admin.blog.add');
+    }
+
+    public function store_blog(Request $request)
+    {
+        // dd($request->all());
+        $save = request()->validate([
+            'title' => 'required',
+            'slug' => 'required',
+            'description' => 'required'
+        ]);
+
+        $save = new BlogModel;
+        $save->title = trim($request->title);
+        $save->slug = trim($request->slug);
+        $save->description = trim($request->description);
+        $save->save();
+
+        return redirect('admin/blog')->with('success', 'Blog Successfully Added');
     }
 }
