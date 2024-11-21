5c5936a07d2a87b6eef1eca9f258f81da87b55fe
kanayofirms <kanyeze@gmail.com>
2024-11-21 21:15:26 +0100

    feat(model): Update getAllRecord method with search and pagination

--- app/Models/BlogModel.php
+++ app/Models/BlogModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class BlogModel extends Model
 {
@@ -14,8 +15,22 @@ class BlogModel extends Model
     static public function getAllRecord()
     {
         $return = self::select('blog.*')
-            ->orderBy('id', 'desc')
-            ->paginate(20);
+            ->orderBy('id', 'desc');
+
+        // Search Box Start
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('blog.id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('title'))) {
+            $return = $return->where('blog.title', 'like', '%' . Request::get('title') . '%');
+        }
+
+        if (!empty(Request::get('slug'))) {
+            $return = $return->where('blog.slug', 'like', '%' . Request::get('slug') . '%');
+        }
+        // Search Box End
+        $return = $return->paginate(20);
 
         return $return;
     }
