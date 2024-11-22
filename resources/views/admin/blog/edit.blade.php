51c72d9cc6764a61d04583c6398dc2aa9b9bc1d0
kanayofirms <kanyeze@gmail.com>
2024-11-22 14:33:18 +0100

    feat(view): Add edit blog view with form for updating blog details

---
+++ resources/views/admin/blog/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('admin.admin_dashboard')
+@section('admin')
+    <div class="page-content">
+        <nav class="page-breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('admin/colour') }}">Blog</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Edit Blog</li>
+            </ol>
+        </nav>
+        <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h6 class="card-title">Edit Blog</h6>
+
+                        <form class="forms-sample" method="POST" action="{{ url('admin/blog/edit/' . $getRecord->id) }}">
+                            {{ csrf_field() }}
+                            <div class="row mb-3">
+                                <label class="col-sm-3 col-form-label">Title <span style="color: red;">*</span></label>
+                                <div class="col-sm-9">
+                                    <input type="text" id="getTitle" class="form-control" name="title"
+                                        value="{{ $getRecord->title }}" placeholder="Enter Title" required>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label class="col-sm-3 col-form-label">Slug <span style="color: red;">*</span><a
+                                        href="javascript:;" id="ConvertSlug">Convert Slug</a></label>
+                                <div class="col-sm-9">
+                                    <input type="text" id="getSlug" class="form-control" name="slug"
+                                        value="{{ $getRecord->slug }}" placeholder="Enter Slug" required>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label class="col-sm-3 col-form-label">Description <span
+                                        style="color: red;">*</span></label>
+                                <div class="col-sm-9">
+                                    <textarea name="description" class="form-control editor" placeholder="Enter Description">{!! $getRecord->description !!}</textarea>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary me-2">Update</button>
+                            <a href="{{ url('admin/blog') }}" class="btn btn-secondary">Back</a>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
