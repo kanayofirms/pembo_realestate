37b552cd1b3fce9ca097ae20bd6801577e085869
kanayofirms <kanyeze@gmail.com>
2024-11-23 05:58:05 +0100

    feat(controller): add countries_index method to display list of countries

---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\CountriesModel;
+
+class LocationController extends Controller
+{
+    public function countries_index(Request $request)
+    {
+        $data['getRecord'] = CountriesModel::get();
+        return view('admin.location.list', $data);
+    }
+}
