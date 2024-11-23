f2bf42c66312a062c47505c5a1ad50aa9aea4b11
kanayofirms <kanyeze@gmail.com>
2024-11-23 06:21:25 +0100

    feat(controller): add countries_store method to save new country

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -17,4 +17,13 @@ class LocationController extends Controller
     {
         return view('admin.countries.add');
     }
+
+    public function countries_store(Request $request)
+    {
+        $save = new CountriesModel;
+        $save->country_name = trim($request->country_name);
+        $save->save();
+
+        return redirect('admin/countries')->with('success', "Country Successfully Added!");
+    }
 }
