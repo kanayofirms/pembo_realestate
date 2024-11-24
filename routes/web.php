56b3c8fd8265bafb82c1676bc1c0c4430783935e
kanayofirms <kanyeze@gmail.com>
2024-11-24 06:41:54 +0100

    feat(route): add route for editing state details

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('admin/state', [LocationController::class, 'state_list']);
     Route::get('admin/state/add', [LocationController::class, 'state_add']);
     Route::post('admin/state/add', [LocationController::class, 'state_store']);
+    Route::get('admin/state/edit/{id}', [LocationController::class, 'state_edit']);
     // Address End
     Route::get('admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
