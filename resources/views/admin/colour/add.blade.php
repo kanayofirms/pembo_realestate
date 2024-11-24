9c77ed91a6d4f09c5cb033d79606038c4bbff9f7
kanayofirms <kanyeze@gmail.com>
2024-11-24 04:53:57 +0100

    fix(view): refactor code for functionality

--- resources/views/admin/colour/add.blade.php
+++ resources/views/admin/colour/add.blade.php
@@ -19,7 +19,8 @@
                             <div class="row mb-3">
                                 <label class="col-sm-3 col-form-label">Name <span style="color: red;">*</span></label>
                                 <div class="col-sm-9">
-                                    <input type="text" class="form-control" name="name" placeholder="Name" required>
+                                    <input type="text" class="form-control" name="name" placeholder="Enter Name"
+                                        required>
                                 </div>
                             </div>
 
