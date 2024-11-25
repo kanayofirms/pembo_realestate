22146d97e49987c8e186d4ea8b6ac79a9fb7f5b7
kanayofirms <kanyeze@gmail.com>
2024-11-25 15:15:05 +0100

    feat(model): add getRecordJoin method in CityModel for fetching city data with country and state joins

---
+++ app/Models/CityModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CityModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'city';
+
+    static public function getRecordJoin()
+    {
+        return self::select('city.*', 'countries.country_name', 'state.state_name')
+            ->join('countries', 'countries.id', '=', 'city.countries_id')
+            ->join('state', 'state.id', '=', 'city.state_id')
+            ->orderBy('city.id', 'desc')
+            ->paginate(20);
+    }
+}
