0b2bdde715769882925f31531b4b480161e8988c
kanayofirms <kanyeze@gmail.com>
2024-11-25 16:11:20 +0100

    feat(model): add dynamic search functionality in  for joined records with pagination

--- app/Models/CityModel.php
+++ app/Models/CityModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class CityModel extends Model
 {
@@ -13,10 +14,29 @@ class CityModel extends Model
 
     static public function getRecordJoin()
     {
-        return self::select('city.*', 'countries.country_name', 'state.state_name')
+        $return = self::select('city.*', 'countries.country_name', 'state.state_name')
             ->join('countries', 'countries.id', '=', 'city.countries_id')
             ->join('state', 'state.id', '=', 'city.state_id')
-            ->orderBy('city.id', 'desc')
-            ->paginate(20);
+            ->orderBy('city.id', 'desc');
+        // Search Start
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('city.id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('country_name'))) {
+            $return = $return->where('countries.country_name', 'like', '%' . Request::get('country_name') . '%');
+        }
+
+        if (!empty(Request::get('state_name'))) {
+            $return = $return->where('state.state_name', 'like', '%' . Request::get('state_name') . '%');
+        }
+
+        if (!empty(Request::get('city_name'))) {
+            $return = $return->where('city.city_name', 'like', '%' . Request::get('city_name') . '%');
+        }
+        // Search End
+
+        $return = $return->paginate(20);
+        return $return;
     }
 }
