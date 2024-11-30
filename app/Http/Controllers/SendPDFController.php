5061748bebc95f180288d5a7153a5a3a8d1bbf55
kanayofirms <kanyeze@gmail.com>
2024-11-30 10:28:43 +0100

    feat(controller): Implemented the send_pdf_sent method to validate PDF upload and email sending functionality. The method validates the input, stores the PDF, and sends it via email using SendPDFMail, with error handling.

--- app/Http/Controllers/SendPDFController.php
+++ app/Http/Controllers/SendPDFController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\SendPDFMail;
+use Mail;
 
 class SendPDFController extends Controller
 {
     public function send_pdf()
     {
         return view('admin.send_pdf.list');
+    }
+
+    public function send_pdf_sent(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            'doc_file' => 'required|file|mimes:pdf|max:2048',
+            'email' => 'required',
+            'subject' => 'required',
+            'message' => 'required',
+        ]);
+        try {
+            $file = $request->file('doc_file');
+            $filePath = $file->store('documents');
+            $fileUrl = asset('storage/app/' . $filePath);
+
+            Mail::to($request->email)->send(new SendPDFMail($request, $filePath, $fileUrl));
+        } catch (\Exception $e) {
+
+        }
+
+        return redirect('admin/send_pdf')->with('success', "Document Successfully Sent!");
     }
 }
