9d841665d847a5d0b1fc83b09232d6d0b09d44e9
kanayofirms <kanyeze@gmail.com>
2024-12-02 06:03:11 +0100

    feat(model): add method to retrieve agent-specific transaction records

--- app/Models/TransactionsModel.php
+++ app/Models/TransactionsModel.php
@@ -10,4 +10,13 @@ class TransactionsModel extends Model
     use HasFactory;
 
     protected $table = 'transactions';
+
+    static public function getAgentRecord($user_id)
+    {
+        return self::select('transactions.*', 'users.name')
+            ->join('users', 'users.id', '=', 'transactions.user_id')
+            ->where('transactions.user_id', '=', $user_id)
+            ->orderBy('transactions.id', 'desc')
+            ->paginate(50);
+    }
 }
