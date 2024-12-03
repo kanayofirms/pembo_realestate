4e613d187dfc06474ece3c2c74ab4dc7fdd35fd8
kanayofirms <kanyeze@gmail.com>
2024-12-03 13:17:46 +0100

    feat(controller): add action method to handle event creation, update, and deletion

--- app/Http/Controllers/FullCalendarController.php
+++ app/Http/Controllers/FullCalendarController.php
@@ -9,6 +9,40 @@ class FullCalendarController extends Controller
 {
     public function full_calendar(Request $request)
     {
+        if ($request->ajax()) {
+            $data = EventModel::whereDate('start', '>=', $request->start)
+                ->whereDate('end', '<=', $request->end)
+                ->get(['id', 'title', 'start', 'end']);
+            return response()->json($data);
+        }
+
         return view('admin.full_calendar.list');
+    }
+
+    public function action(Request $request)
+    {
+        if ($request->ajax()) {
+            if ($request->type == 'add') {
+                $event = EventModel::create([
+                    'title' => $request->title,
+                    'start' => $request->start,
+                    'end' => $request->end
+                ]);
+                return response()->json($event);
+            }
+            if ($request->type == 'update') {
+                $event = EventModel::find($request->id)->update([
+                    'title' => $request->title,
+                    'start' => $request->start,
+                    'end' => $request->end
+                ]);
+                return response()->json($event);
+            }
+            if ($request->type == 'delete') {
+                $event = EventModel::find($request->id)->delete();
+
+                return response()->json($event);
+            }
+        }
     }
 }
