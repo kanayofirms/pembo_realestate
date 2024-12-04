f33a466378c40fe813d01a4c4be9fa209d2c32a0
kanayofirms <kanyeze@gmail.com>
2024-12-04 05:35:59 +0100

    feat(ui): integrate SweetAlert confirmation for delete actions in agent dashboard

--- resources/views/agent/agent_dashboard.blade.php
+++ resources/views/agent/agent_dashboard.blade.php
@@ -80,8 +80,30 @@
     <script src="{{ asset('assets/js/dashboard-dark.js') }}"></script>
     <!-- End custom js for this page -->
 
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     @yield('script')
-    <script type="text/javascript"></script>
+    <script type="text/javascript">
+        $('.btn-delete').click(function(e) {
+            e.preventDefault();
+            var form = $(this).parents("form");
+
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.submit();
+                }
+            });
+        });
+    </script>
+
 
 </body>
 
