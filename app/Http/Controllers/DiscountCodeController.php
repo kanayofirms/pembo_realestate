cefcb4b725c57cd07fae6ba958e187eacfb7a856
kanayofirms <kanyeze@gmail.com>
2024-12-04 21:40:27 +0100

    feat(controller): add functionality to store discount code

--- app/Http/Controllers/DiscountCodeController.php
+++ app/Http/Controllers/DiscountCodeController.php
@@ -3,16 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\DiscountCodeModel;
+use Auth;
 
 class DiscountCodeController extends Controller
 {
     public function discount_code()
     {
-        return view('admin.discount_code.list');
+        $data['getRecord'] = DiscountCodeModel::get();
+        return view('admin.discount_code.list', $data);
     }
 
     public function discount_code_add()
     {
         return view('admin.discount_code.add');
+    }
+
+    public function discount_code_store(Request $request)
+    {
+        $save = new DiscountCodeModel;
+        $save->user_id = Auth::user()->id;
+        $save->discount_code = trim($request->discount_code);
+        $save->discount_price = trim($request->discount_price);
+        $save->expiry_date = $request->expiry_date;
+        $save->type = $request->type;
+        $save->usages = $request->usages;
+        $save->save();
+
+        return redirect('admin/discount_code')->with('success', 'Discount Code Successfully Saved!');
     }
 }
