243d6225f9578d2f56de894fe5675958b013f970
kanayofirms <kanyeze@gmail.com>
2024-12-04 05:46:32 +0100

    feat(admin): add breadcrumb link to city management in city addition view

--- resources/views/admin/city/add.blade.php
+++ resources/views/admin/city/add.blade.php
@@ -3,7 +3,7 @@
     <div class="page-content">
         <nav class="page-breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ url('admin/countries') }}">City</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('admin/city') }}">City</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Add City</li>
             </ol>
         </nav>
