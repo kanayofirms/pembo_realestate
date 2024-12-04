e640051f33b32f7c97975034dfff26023a908c18
kanayofirms <kanyeze@gmail.com>
2024-12-04 22:51:54 +0100

    feat(model): add method to retrieve and paginate discount code records with user info

--- app/Models/DiscountCodeModel.php
+++ app/Models/DiscountCodeModel.php
@@ -10,4 +10,15 @@ class DiscountCodeModel extends Model
     use HasFactory;
 
     protected $table = 'discount_code';
+
+    static public function getAllRecord()
+    {
+        $return = self::select('discount_code.*', 'users.name')
+            ->join('users', 'users.id', '=', 'discount_code.user_id');
+
+        $return = $return->orderBy('discount_code.id', 'desc')
+            ->paginate(20);
+
+        return $return;
+    }
 }
