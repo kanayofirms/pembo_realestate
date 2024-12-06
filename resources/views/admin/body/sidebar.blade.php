ed5306c3ae6ebb798d73c2d725868b0f74abd16e
kanayofirms <kanyeze@gmail.com>
2024-12-06 02:35:49 +0100

    feat(admin-sidebar): add support link to sidebar

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -82,6 +82,13 @@
                  </a>
              </li>
 
+             <li class="nav-item">
+                 <a href="{{ url('admin/support') }}" class="nav-link">
+                     <i class="link-icon" data-feather="box"></i>
+                     <span class="link-title">Support</span>
+                 </a>
+             </li>
+
              <li class="nav-item nav-category">User Week</li>
              <li class="nav-item" @if (Request::segment(2) == 'week') active @endif>
                  <a href="{{ url('admin/week') }}" class="nav-link">
