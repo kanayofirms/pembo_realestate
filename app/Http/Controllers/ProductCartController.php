ab9ac42d9019ad14698f5f755bb33a94e6583ec0
kanayofirms <kanyeze@gmail.com>
2024-12-12 01:48:04 +0100

    feat(controller): add admin_product_store method to handle product addition

--- app/Http/Controllers/ProductCartController.php
+++ app/Http/Controllers/ProductCartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ProductCartModel;
+use Str;
 
 class ProductCartController extends Controller
 {
@@ -15,5 +16,23 @@ class ProductCartController extends Controller
     public function admin_product_add()
     {
         return view('admin.product_cart.add');
+    }
+
+    public function admin_product_store(Request $request)
+    {
+        $save = new ProductCartModel;
+        $save->name = trim($request->name);
+        $save->description = trim($request->description);
+        $save->price = trim($request->price);
+
+        if (!empty($request->file('image'))) {
+            $file = $request->file('image');
+            $randomStr = Str::random(30);
+            $filename = $randomStr . '.' . $file->getClientOriginalExtension();
+            $file->move('product/', $filename);
+            $save->image = $filename;
+        }
+        $save->save();
+        return redirect('admin/product_cart')->with('success', "Product Successfully Added!");
     }
 }
